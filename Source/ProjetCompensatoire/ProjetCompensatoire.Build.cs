8065088ee3fcabee416cd0c19d532cad53ea9840
Hinenao <hinenaolamberty@outlook.fr>
2025-09-02 23:42:30 +0200

    tetertretert

--- Source/ProjetCompensatoire/ProjetCompensatoire.Build.cs
+++ Source/ProjetCompensatoire/ProjetCompensatoire.Build.cs
@@ -9,5 +9,6 @@ public class ProjetCompensatoire : ModuleRules
 		PCHUsage = PCHUsageMode.UseExplicitOrSharedPCHs;
 
 		PublicDependencyModuleNames.AddRange(new string[] { "Core", "CoreUObject", "Engine", "InputCore", "EnhancedInput" });
+		PrivateDependencyModuleNames.AddRange(new string[] { "GameplayTasks", "GameplayTags", "GameplayAbilities" });
 	}
 }
